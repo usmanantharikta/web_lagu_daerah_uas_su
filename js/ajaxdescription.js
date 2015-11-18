876225c2d748bbe4098c33d9dd526fe436672235
usmanantharikta <antharikta@gmail.com>
2015-11-18 22:00:55 +0700

    add ajax jakarta and jampi

--- js/ajaxdescription.js
+++ js/ajaxdescription.js
@@ -110,3 +110,193 @@ xhttp.onreadystatechange = function() {
 xhttp.open("GET", "pages/jabar/Pileuleuyan.html", true);
 xhttp.send();
 }
+
+
+//<!--1. ajax SangBango description -->
+//  <script>
+function SangBango() {
+  var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("desciption").innerHTML = xhttp.responseText;
+  }
+};
+xhttp.open("GET", "pages/jakarta/SangBango.html", true);
+xhttp.send();
+}
+//</script>
+
+//<!-- 2. ajax Bubuybulan description -->
+//  <script>
+function KicirKicir() {
+  var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("desciption").innerHTML = xhttp.responseText;
+  }
+};
+xhttp.open("GET", "pages/jakarta/KicirKicir.html", true);
+xhttp.send();
+}
+//</script>
+
+//<!--3. ajax Es lilin description -->
+//  <script>
+function KeroncongKemayoran() {
+  var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("desciption").innerHTML = xhttp.responseText;
+  }
+};
+xhttp.open("GET", "pages/jakarta/KeroncongKemayoran.html", true);
+xhttp.send();
+}
+//</script>
+
+//4. ajax cingcangkling
+function HujanGerimis() {
+  var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("desciption").innerHTML = xhttp.responseText;
+  }
+};
+xhttp.open("GET", "pages/jakarta/HujanGerimis.html", true);
+xhttp.send();
+}
+//5. ajax PanonHideung
+function JaliJali() {
+  var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("desciption").innerHTML = xhttp.responseText;
+  }
+};
+xhttp.open("GET", "pages/jakarta/JaliJali.html", true);
+xhttp.send();
+}
+
+//6. ajax WarungPojok
+function LenggangLenggangKangkung() {
+  var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("desciption").innerHTML = xhttp.responseText;
+  }
+};
+xhttp.open("GET", "pages/jakarta/LenggangLenggangKangkung.html", true);
+xhttp.send();
+}
+
+//7. ajax tokecang
+function OndelOndel() {
+  var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("desciption").innerHTML = xhttp.responseText;
+  }
+};
+xhttp.open("GET", "pages/jakarta/OndelOndel.html", true);
+xhttp.send();
+}
+
+//ajax jambiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+function MajeuKincai() {
+  var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("desciption").innerHTML = xhttp.responseText;
+  }
+};
+xhttp.open("GET", "pages/jambi/MajeuKincai.html", true);
+xhttp.send();
+}
+//</script>
+
+//<!-- 2. ajax Bubuybulan description -->
+//  <script>
+function DodoiSiDodoi() {
+  var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("desciption").innerHTML = xhttp.responseText;
+  }
+};
+xhttp.open("GET", "pages/jambi/DodoiSiDodoi.html", true);
+xhttp.send();
+}
+//</script>
+
+//<!--3. ajax Es lilin description -->
+//  <script>
+function BatangHari() {
+  var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("desciption").innerHTML = xhttp.responseText;
+  }
+};
+xhttp.open("GET", "pages/jambi/Batanghari.html", true);
+xhttp.send();
+}
+//</script>
+
+//4. ajax cingcangkling
+function KetimunBungkuk() {
+  var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("desciption").innerHTML = xhttp.responseText;
+  }
+};
+xhttp.open("GET", "pages/jambi/KetimunBungkuk.html", true);
+xhttp.send();
+}
+//5. ajax PanonHideung
+function InjitInjitSemut() {
+  var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("desciption").innerHTML = xhttp.responseText;
+  }
+};
+xhttp.open("GET", "pages/jambi/InjitInjitSemut.html", true);
+xhttp.send();
+}
+
+//6. ajax WarungPojok
+function TanjungBajure() {
+  var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("desciption").innerHTML = xhttp.responseText;
+  }
+};
+xhttp.open("GET", "pages/jambi/TanjungBajure.html", true);
+xhttp.send();
+}
+
+//7. ajax tokecang
+function OrangKayoHitam() {
+  var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("desciption").innerHTML = xhttp.responseText;
+  }
+};
+xhttp.open("GET", "pages/jambi/OrangKayoHitam.html", true);
+xhttp.send();
+}
+
+//8. ajax bajing luncat
+function TimangTimangAnakkuSayang() {
+  var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("desciption").innerHTML = xhttp.responseText;
+  }
+};
+xhttp.open("GET", "pages/jambi/TimangTimangAnakkuSayang.html", true);
+xhttp.send();
+}
